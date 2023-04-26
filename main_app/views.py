bd3a380d321214a8e963d2f09a8470634ed3e115
regananderson929 <92687313+regananderson929@users.noreply.github.com>
2023-04-25 23:49:00 -0400

    adds total amount and value to different views

--- main_app/views.py
+++ main_app/views.py
@@ -5,16 +5,44 @@ from .models import Asset
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    assets = Asset.objects.all()
+    total_amount = sum(asset.amount for asset in assets)
+    total_value = sum(asset.value for asset in assets)
+    context = {
+        'assets': assets,
+        'total_amount': total_amount,
+        'total_value': total_value,
+    }
+    return render(request, 'home.html', context)
+
+# def assets_index(request):
+#     assets = Asset.objects.all()
+#     return render(request, 'assets/index.html', {'assets': assets})
 
 def assets_index(request):
     assets = Asset.objects.all()
-    return render(request, 'assets/index.html', {'assets': assets})
+   
+    return render(request, 'assets/index.html', {'assets':assets})
 
 def assets_detail(request):
     assets = Asset.objects.all()
-    return render(request, 'assets/detail.html', {'assets': assets})
+    total_amount = sum(asset.amount for asset in assets)
+    total_value = sum(asset.value for asset in assets)
+    context = {
+        'assets': assets,
+        'total_amount': total_amount,
+        'total_value': total_value,
+    }
+    return render(request, 'assets/detail.html', context)
 
 def asset_type(request, asset_type):
     assets = Asset.objects.filter(type=asset_type)
-    return render(request, 'assets/list.html', {'assets': assets})
+    total_amount = sum(asset.amount for asset in assets)
+    total_value = sum(asset.value for asset in assets)
+    context = {
+        'assets': assets,
+        'total_amount': total_amount,
+        'total_value': total_value,
+    }
+    return render(request, 'assets/list.html', context)
+
