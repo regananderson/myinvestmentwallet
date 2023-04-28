9558246fe011adfcef2288a310f62c289cc38341
regananderson929 <92687313+regananderson929@users.noreply.github.com>
2023-04-27 20:09:41 -0400

    adds login requirement

--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,11 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Asset
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-
+@login_required
 def home(request):
     assets = Asset.objects.all()
     total_amount = sum(asset.amount for asset in assets)
@@ -16,14 +18,13 @@ def home(request):
         'total_value': total_value,
     }
     return render(request, 'home.html', context)
-
+@login_required
 def assets_index(request):
-    assets = Asset.objects.all()
-   
+    assets = Asset.objects.filter(user=request.user)
     return render(request, 'assets/index.html', {'assets':assets})
-
+@login_required
 def assets_detail(request):
-    assets = Asset.objects.all()
+    assets = Asset.objects.filter(user=request.user)
     total_amount = sum(asset.amount for asset in assets)
     total_value = sum(asset.value for asset in assets)
     context = {
@@ -32,7 +33,7 @@ def assets_detail(request):
         'total_value': total_value,
     }
     return render(request, 'assets/detail.html', context)
-
+@login_required
 def asset_type(request, asset_type):
     assets = Asset.objects.filter(type=asset_type)
     total_amount = sum(asset.amount for asset in assets)
@@ -43,7 +44,7 @@ def asset_type(request, asset_type):
         'total_value': total_value,
     }
     return render(request, 'assets/list.html', context)
-
+@login_required
 def investments_detail(request, asset_id):
     asset = Asset.objects.get(id=asset_id)
     return render(request, 'investments/detail.html', {'asset':asset})
@@ -64,7 +65,7 @@ def signup(request):
         'error': error_message
         })
 
-class AssetCreate(CreateView):
+class AssetCreate(LoginRequiredMixin, CreateView):
     model = Asset
     fields = ('type', 'name', 'amount', 'description','value')
     template_name = 'assets/main_app/asset_form.html'
@@ -72,12 +73,12 @@ class AssetCreate(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class AssetUpdate(UpdateView):
+class AssetUpdate(LoginRequiredMixin, UpdateView):
     model = Asset
     fields = '__all__'
     template_name = 'assets/main_app/asset_form.html'
 
-class AssetDelete(DeleteView):
+class AssetDelete(LoginRequiredMixin, DeleteView):
     model = Asset
     success_url = '/assets/'
     template_name = 'assets/main_app/asset_confirm_delete.html'
